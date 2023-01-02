eb1b67d2cfe1411f9c6cdda8ea0b765a7626b8a6
Bulat Murtazin <bulaft.murtazin@gmail.com>
2023-01-02 23:16:36 +0600

    Setup ESLint/Prettier in `frontend` app

---
+++ apps/frontend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
---
+++ apps/frontend/.prettierrc.js
@@ -0,0 +1,6 @@
+const prettierConfigShared = require('prettier-config-shared');
+
+module.exports = {
+  ...prettierConfigShared,
+  vueIndentScriptAndStyle: true,
+};
