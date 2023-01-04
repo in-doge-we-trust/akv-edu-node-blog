07fafaca5b3b8e46a0f499eb79252ab4b048c2f4
Bulat Murtazin <bulaft.murtazin@gmail.com>
2023-01-05 01:56:14 +0600

    Build script cleanup (`backend` app)

--- apps/backend/scripts/build.js
+++ apps/backend/scripts/build.js
@@ -3,40 +3,73 @@
 const path = require('path');
 const fs = require('fs');
 const chalk = require('chalk');
+const esbuild = require('esbuild');
 
-const distDir = path.join(__dirname, '..', 'dist');
+const cleanBuildFolder = () => {
+  const distDir = path.join(__dirname, '..', 'dist');
 
-console.log(
-  chalk.blue(`Checking whether build folder (${distDir}) exists or not...`),
-);
-const isBuildFolderExist = fs.existsSync(distDir);
+  console.log('');
+  console.log(
+    chalk.blue(`Checking whether build folder (${distDir}) exists or not...`),
+  );
+  console.log('');
 
-if (isBuildFolderExist) {
-  console.log(chalk.yellow('Found existing build folder!'));
+  const isBuildFolderExist = fs.existsSync(distDir);
+
+  if (isBuildFolderExist) {
+    console.log(chalk.yellow('Found existing build folder!'));
+
+    console.log('');
+    console.log('------------------------------------');
+    console.log('');
+
+    console.log(chalk.blue('Cleaning the build folder...'));
+    console.log('');
+
+    fs.rmSync(distDir, { recursive: true });
+
+    console.log(chalk.green('Cleaned build folder successfully!'));
+  } else {
+    console.log(chalk.blue('Build folder was not found.'));
+  }
+};
+
+const copyEnvFile = () => {
+  console.log('');
+  console.log('------------------------------------');
+  console.log('');
+
+  console.log(chalk.blue('Copying .env file...'));
+
+  try {
+    fs.copyFileSync(
+      path.join(__dirname, '..', '.env'),
+      path.join(__dirname, '..', 'dist', '.env'),
+      fs.constants.COPYFILE_FICLONE,
+    );
+  } catch (err) {
+    console.log(chalk.red('An error occurred while copying .env file!'));
+    console.error(err);
+
+    process.exit(1);
+  }
+
+  console.log('');
+  console.log(chalk.green('Copied .env file to build directory successfully!'));
+  console.log('');
+};
+
+const build = async () => {
+  cleanBuildFolder();
 
   console.log('');
   console.log('------------------------------------');
   console.log('');
 
-  console.log(chalk.blue('Cleaning the build folder...'));
+  console.log(chalk.blue('Starting the build...'));
+  console.log('');
 
-  fs.rmSync(distDir, { recursive: true });
-
-  console.log(chalk.green('Cleaned build folder successfully!'));
-}
-
-console.log(chalk.blue('Build folder was not found.'));
-
-console.log('');
-console.log('------------------------------------');
-console.log('');
-
-console.log(chalk.blue('Starting the build...'));
-
-const esbuild = require('esbuild');
-
-esbuild
-  .build({
+  await esbuild.build({
     entryPoints: ['src/app.ts'],
     bundle: true,
     minify: true,
@@ -45,36 +78,12 @@ esbuild
     target: 'node18',
     outdir: './dist',
     logLevel: 'info',
-  })
-  .then(() => {
-    console.log('');
-    console.log('------------------------------------');
-    console.log('');
-
-    console.log(chalk.green('Build succeeded!'));
-  })
-  .then(() => {
-    console.log('');
-    console.log('------------------------------------');
-    console.log('');
-
-    console.log(chalk.blue('Copying .env file...'));
-
-    try {
-      fs.copyFileSync(
-        path.join(__dirname, '..', '.env'),
-        path.join(__dirname, '..', 'dist', '.env'),
-        fs.constants.COPYFILE_FICLONE,
-      );
-    } catch (err) {
-      console.log(chalk.red('An error occurred while copying .env file!'));
-      console.error(err);
-
-      process.exit(1);
-    }
-
-    console.log('');
-    console.log(
-      chalk.green('Copied .env file to build directory successfully!'),
-    );
   });
+
+  console.log('');
+  console.log(chalk.green('<<<< Build succeeded! >>>>'));
+
+  copyEnvFile();
+};
+
+build();
