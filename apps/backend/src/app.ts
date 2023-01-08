7bad0b38f40d89cf88f8d5f8a443dc6d1f7a6c11
Bulat Murtazin <bulaft.murtazin@gmail.com>
2023-01-08 19:55:31 +0600

    Move .env handling to `src/config/env.ts`

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,50 +1,17 @@
-import dotenv from 'dotenv';
 import Fastify from 'fastify';
 import chalk from 'chalk';
-import path from 'path';
 
-// SETTING UP ENV
-
-// <<<< START >>>>
-const { NODE_ENV = 'production' } = process.env;
-console.log(chalk.yellow(`Environment type is set to "${NODE_ENV}"!`));
-
-let envFileName = '';
-switch (NODE_ENV) {
-  case 'development':
-    envFileName = '.env.local';
-    break;
-  default:
-    envFileName = '.env';
-}
-
-let envFilePath = '';
-switch (NODE_ENV) {
-  case 'development':
-    envFilePath = path.join(__dirname, '..', envFileName);
-    break;
-  default:
-    envFilePath = path.join(__dirname, envFileName);
-}
-
-console.log({ envFilePath });
-
-dotenv.config({
-  path: envFilePath,
-});
-// <<<< END >>>>
+import { PORT } from './config/env';
 
 const app = Fastify({
   logger: true,
 });
 
-app.get('/', (req, reply) => {
+app.get('/', (_req, reply) => {
   reply.status(200).send({
     message: 'Welcome to Akv Edu Node Blog app API, stranger!',
   });
 });
-
-const PORT = process.env.PORT as unknown as number;
 
 app.listen(
   {
