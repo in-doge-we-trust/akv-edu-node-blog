f9268752e02fb6e7c98c6ae2c05e3bc182c977cb
Bulat Murtazin <bulatf.murtazin@gmail.com>
2023-01-14 22:00:19 +0600

    `backend` - upd config/env.ts to use pg-related variables

--- apps/backend/src/config/env.ts
+++ apps/backend/src/config/env.ts
@@ -3,3 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = process.env['PORT'] ? Number(process.env['PORT']) : 8080;
+
+export const POSTGRES_USER = process.env['POSTGRES_USER'] ?? '<username>';
+export const POSTGRES_PASSWORD =
+  process.env['POSTGRES_PASSWORD'] ?? '<password>';
+export const POSTGRES_PORT = process.env['POSTGRES_PORT']
+  ? Number(process.env['POSTGRES_PORT'])
+  : 5432;
+export const POSTGRES_DB = process.env['POSTGRES_DB'] ?? 'akv_edu_node_blog_db';
