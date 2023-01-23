549d4bce419314d436f672a15c8bc2b787c8d717
Bulat Murtazin <bulatf.murtazin@gmail.com>
2023-01-23 12:30:38 +0600

    Upd post schema

--- packages/core-lib/src/schemas/post.ts
+++ packages/core-lib/src/schemas/post.ts
@@ -6,4 +6,6 @@ export const PostSchemaShape = z.object({
   id: IdSchema,
   title: z.string(),
   content: z.string(),
+  createdAt: z.string().datetime(),
+  updatedAt: z.string().datetime(),
 });
